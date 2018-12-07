e97164d3f61a711ec6173fd02dff5dc2227524ae
amritsiidhu <amritsidhu360@gmail.com>
2018-12-07 23:08:06 +0000

    Added tests to check correct contentd are being displayed

--- tests/app/main/views/test_index.py
+++ tests/app/main/views/test_index.py
@@ -6,17 +6,34 @@ from flask import json, url_for, request
 from tests.conftest import AUTH_USERNAME, AUTH_PASSWORD
 
 
-''' this test subject to change
 class WhenAccessingHomePage(object):
-
-
-    def it_shows_list_of_available_pages(self, client, mocker):
+    def it_should__display_banner_text(self, client, mocker):
         response = client.get(url_for(
             'main.index'
         ))
         page = BeautifulSoup(response.data.decode('utf-8'), 'html.parser')
-        assert 'Reg. Charity no. 1110140' in page.find('p').text
-'''
+        content = page.find("div", {"class": "banner_findoutmore"}).string
+        assert content == 'FIND OUT MORE'
+
+
+class WhenAccessingHomePage(object):
+    def it_should_display_header_content(self, client, mocker):
+        response = client.get(url_for(
+            'main.index'
+        ))
+        page = BeautifulSoup(response.data.decode('utf-8'), 'html.parser')
+        content = page.find("div", {"class": "header_about"}).string
+        assert content == 'About'
+
+
+class WhenAccessingHomePage(object):
+    def it_should_display_footer_content(self, client, mocker):
+        response = client.get(url_for(
+            'main.index'
+        ))
+        page = BeautifulSoup(response.data.decode('utf-8'), 'html.parser')
+        content = page.find("div", {"class": "footer_whatweoffer"}).string
+        assert content == 'What we offer'
 
 
 class WhenAccessingPagesWithoutLoggingIn(object):
